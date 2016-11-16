c86e6e36d6444c312b774a1bf26132b94a8d45f3
Ryan White <ry.white@gmail.com>
2016-11-16 21:19:37 +0000

    First commit

--- toolbox2/main.cpp
+++ toolbox2/main.cpp
@@ -1,15 +1,108 @@
-//
-//  main.cpp
-//  toolbox2
-//
-//  Created by Ryan White on 15/11/2016.
-//  Copyright (c) 2016 Ryan White. All rights reserved.
-//
-
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace cv;
+using namespace std;
+
+Mat src; Mat src_gray;
+int thresh = 100;
+int max_thresh = 255;
+RNG rng(12345);
+
+
+int morph_elem = 0;
+int morph_size = 0;
+int morph_operator = 0;
+int const max_operator = 4;
+int const max_elem = 2;
+int const max_kernel_size = 21;
+
+
+
+/// Function header
+void thresh_callback(int, void* );
+
+/** @function main */
+int main( int argc, char** argv )
+{
+    /// Load source image and convert it to gray
+    src = imread("test.bmp", 1 );
+    
+    /// Convert image to gray and blur it
+    cvtColor( src, src_gray, CV_BGR2GRAY );
+    blur( src_gray, src_gray, Size(3,3) );
+ //   bitwise_not(src_gray,src_gray);
+    
+    morph_size = 2;
+    
+    Mat element = getStructuringElement( 2, Size( 2*morph_size + 1, 2*morph_size+1 ), Point( morph_size, morph_size ) );
+
+    
+    morphologyEx( src_gray, src_gray, 1, element );
+
+    
+    /// Create Window
+    char* source_window = "Source";
+    namedWindow( source_window, CV_WINDOW_NORMAL );
+    imshow( source_window, src_gray );
+    
+    createTrackbar( " Canny thresh:", "Source", &thresh, max_thresh, thresh_callback );
+    thresh_callback( 0, 0 );
+    
+    waitKey(0);
+    return(0);
+}
+
+/** @function thresh_callback */
+void thresh_callback(int, void* )
+{
+    Mat canny_output;
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    
+    /// Detect edges using canny
+    Canny( src_gray, canny_output, thresh, thresh*2, 3 );
+    /// Find contours
+    findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+    
+    /// Draw contours
+    Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+    
+    int i = contours.size();
+    Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+    drawContours( drawing, contours, i, color, 1, 8, hierarchy, 0, Point() );
+    
+//    
+//    for( int i = 0; i< contours.size(); i++ )
+//    {
+//        
+//        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+//        drawContours( drawing, contours, i, color, 1, 8, hierarchy, 0, Point() );
+//   //     imshow("Contours",drawing);
+//  //      cout << "Heirarchy \n " << hierarchy[i];
+//    //    waitKey(0);
+//        
+//
+//        // look for hierarchy[i][3]!=-1, ie hole boundaries
+////        if ( hierarchy[i][1] != -1 ) {
+////            cout << "hiearachy \n" << hierarchy[i][1];
+////            // random colour
+////            Scalar colour( (rand()&255), (rand()&255), (rand()&255) );
+////            drawContours( drawing, contours, i, colour );
+////            imshow( "Contours", drawing);
+////            
+////            waitKey(0);
+////            
+//        
+//            
+////        }
+    
+//}
+
+    /// Show in a window
+    namedWindow( "Contours", CV_WINDOW_NORMAL );
+    imshow( "Contours", drawing );
 }
