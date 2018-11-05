b2190bce45faad4952bf12f09d5e7ffaac9fc113
Ryan White <ry.white@gmail.com>
2018-11-05 04:51:08 +0000

    Found decent coefficients for edge detection and kernel size

--- toolbox2/optimize.py
+++ toolbox2/optimize.py
@@ -34,73 +34,98 @@ MSEs = []
 blursize = 3
 threshold = 0 # When using otsu binarization, threshold is automatically determined
 
-thresholda = 100
-thresholdb = 200
-
-for cropped_imagename in cropped_images:
-    #print("processing: ", cropped_imagename)
-
-    _, imagename = cropped_imagename.split('/')
-
-    cropped_image = cv2.imread(cropped_imagename)
-    cv2.imshow("Cropped Input", cropped_image)
-
-    # Read in manually thresholded image
-
-    manual_imagename = "manual_thresh/" + imagename
-
-    manual_image = cv2.imread(manual_imagename)
-    cv2.imshow("Manual Input", manual_image)
-    manual_image = cv2.cvtColor(manual_image, cv2.COLOR_BGR2GRAY)
-
-    # Redo the threshold, just to make sure it's in the right colour space etc
-    retval, manual_image_threshold = cv2.threshold(manual_image, 50, 255, cv2.THRESH_BINARY_INV)
+# all signs point to these being the best coefficients
+thresholda = 50
+thresholdb = 70
+kernel_size = 7
 
 
-    # This is where to do all the things
-    #                cropped_image = cv2.pyrMeanShiftFiltering(cropped_image, mean_SP, mean_SR, mean_MaxLevel)
+for kernel_size in range(1,15,2):
+#    for thresholda in range(50,200,20):
+#            for thresholdb in range(thresholda,250,20):
 
-    kernel = np.ones((3,3),np.uint8)
+        MSEs = []
 
-    edges = cv2.Canny(cropped_image,100,200)
+        for cropped_imagename in cropped_images:
+            #print("processing: ", cropped_imagename)
 
-    cv2.imshow("edges",edges)
+            _, imagename = cropped_imagename.split('/')
 
-# edges = cv2.morphologyEx(edges,cv2.MORPH_OPEN,kernel)
-    edges = cv2.morphologyEx(edges,cv2.MORPH_CLOSE,kernel)
+            cropped_image = cv2.imread(cropped_imagename)
+        #    cv2.imshow("Cropped Input", cropped_image)
 
-    cv2.imshow("opened closed",edges)
+            # Read in manually thresholded image
 
-    cropped_image = cv2.GaussianBlur(cropped_image, (blursize, blursize), 0)
-    cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
+            manual_imagename = "manual_thresh/" + imagename
 
-    retval, cropped_image_threshold = cv2.threshold(cropped_image, threshold, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+            manual_image = cv2.imread(manual_imagename)
+        #    cv2.imshow("Manual Input", manual_image)
+            manual_image = cv2.cvtColor(manual_image, cv2.COLOR_BGR2GRAY)
 
-    cropped_image_cont, cropped_contours, cropped_hierarchy = cv2.findContours(edges,
-                                                                               cv2.RETR_EXTERNAL,
-                                                                               cv2.CHAIN_APPROX_NONE)
-    largest_area = 0
-    largest_contour_index = 0
+            # Redo the threshold, just to make sure it's in the right colour space etc
+            retval, manual_image_threshold = cv2.threshold(manual_image, 50, 255, cv2.THRESH_BINARY_INV)
 
-    for index, contour in enumerate(cropped_contours):
-        area = cv2.contourArea(contour)
-        if (area > largest_area):
-            largest_area = area
-            largest_contour_index = index
+            manual_image_cont, manual_contours, manual_hierarchy = cv2.findContours(manual_image_threshold,
+                                                                                       cv2.RETR_EXTERNAL,
+                                                                                       cv2.CHAIN_APPROX_NONE)
+            largest_area = 0
+            largest_contour_index = 0
 
-    filled_contours = np.zeros_like(cropped_image_cont)
-    cv2.drawContours(filled_contours, cropped_contours, largest_contour_index, 255, -1)
-    cv2.imshow("Cropped Contour",filled_contours)
+            for index, contour in enumerate(manual_contours):
+                area = cv2.contourArea(contour)
+                if (area > largest_area):
+                    largest_area = area
+                    largest_contour_index = index
 
-    # Stop doing all the things and measure
-    current_MSE = mse(filled_contours, manual_image_threshold)
-    MSEs.append(current_MSE)
+            filled_manual_contours = np.zeros_like(manual_image_cont)
+            cv2.drawContours(filled_manual_contours, manual_contours, largest_contour_index, 255, -1)
+            cv2.imshow("Filled Manual Contour",filled_manual_contours)
 
-    print("MSE: ",current_MSE)
-    cv2.imshow("Manual Image Thresholded", manual_image_threshold)
-    cv2.imshow("Cropped Image Thresholded", cropped_image_threshold)
-    cv2.waitKey(0)
-print("Thresholda",thresholda,"thresholdb",thresholdb,"max mse",max(MSEs),"mean mse",np.mean(MSEs))
+
+            # This is where to do all the things
+            #                cropped_image = cv2.pyrMeanShiftFiltering(cropped_image, mean_SP, mean_SR, mean_MaxLevel)
+
+            kernel = np.ones((kernel_size,kernel_size),np.uint8)
+
+            edges = cv2.Canny(cropped_image,thresholda,thresholdb)
+
+        #    cv2.imshow("edges",edges)
+
+        # edges = cv2.morphologyEx(edges,cv2.MORPH_OPEN,kernel)
+            edges = cv2.morphologyEx(edges,cv2.MORPH_CLOSE,kernel)
+
+        #    cv2.imshow("opened closed",edges)
+
+            cropped_image = cv2.GaussianBlur(cropped_image, (blursize, blursize), 0)
+            cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
+
+         #   retval, cropped_image_threshold = cv2.threshold(cropped_image, threshold, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+            cropped_image_cont, cropped_contours, cropped_hierarchy = cv2.findContours(edges,
+                                                                                       cv2.RETR_EXTERNAL,
+                                                                                       cv2.CHAIN_APPROX_NONE)
+            largest_area = 0
+            largest_filled_contour_index = 0
+
+            for index, contour in enumerate(cropped_contours):
+                area = cv2.contourArea(contour)
+                if (area > largest_area):
+                    largest_area = area
+                    largest_filled_contour_index = index
+
+            filled_contours = np.zeros_like(cropped_image_cont)
+            cv2.drawContours(filled_contours, cropped_contours, largest_filled_contour_index, 255, -1)
+            cv2.imshow("Cropped Contour",filled_contours)
+
+            # Stop doing all the things and measure
+            current_MSE = mse(filled_contours, filled_manual_contours)
+            MSEs.append(current_MSE)
+
+            #print("MSE: ",current_MSE)
+            cv2.imshow("Manual Image Thresholded", manual_image_threshold)
+#            cv2.imshow("Cropped Image Thresholded", cropped_image_threshold)
+            #cv2.waitKey(0)
+        print("Thresholda",thresholda,"thresholdb",thresholdb,"kernelsize",kernel_size,"Sum MSE",sum(MSEs),"max mse",max(MSEs),"mean mse",np.mean(MSEs))
 
 
 
